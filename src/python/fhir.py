ee025abbea0c6bb90abb7e7b4751d130dfc04140
Nick Giangreco <npg2108@cumc.columbia.edu>
2020-05-29 11:47:13 -0400

    fhir script

--- src/python/fhir.py
+++ src/python/fhir.py
@@ -1,9 +1,45 @@
 from fhirclient import client
-settings = {
-'app_id' : 'interop_pit',
-'app_secret' : 'aW50ZXJvcF9waXQ6VkwybExZNUhoTFpVOHozak02VkJvQ1d0NFMyRDlsZkZWSTVX',
-'api_base' : 'https://v8xnb6p8-inter.interopland.com/five-lakes-health-system/fhir/',
-'password' : 'VL2lLY5HhLZU8z3jM6VBoCWt4S2D9lfFVI5W'
-}
-smart = client.FHIRClient(settings=settings)
-import fhirclient.models.patient as p
+import json
+
+class Patient:
+
+	def __init__(self,id="SarahThompson"):
+
+		self.id = id
+		self.base_url = "https://v8xnb6p8-inter.interopland.com/"
+		self.fivelakes_url = "five-lakes-health-system/fhir/"
+		self.payload = {}
+		self.headers = {
+		'Authorization': 'Basic aW50ZXJvcF9waXQ6VkwybExZNUhoTFpVOHozak02VkJvQ1d0NFMyRDlsZkZWSTVX'
+		}
+		self.getPatientData()
+
+	def makeResourceUrl(self,resource="Careplan"):
+
+		return self.base_url+self.fivelakes_url+resource+"?_pretty=true"
+
+	def getPatientData(self):
+
+		resource = "Patient/"+self.id+"/_history/1"
+		response = requests.request("GET", self.makeResourceUrl(resource=resource), headers=self.headers, data = self.payload)
+		parsed = response.json()
+		self.patient_data = parsed
+
+	def extractPatientDemographics(self):
+
+		dem_dict = {
+		'Given name' : ' '.join(self.patient_data['name'][0]['given']),
+		"Family name" : self.patient_data['name'][0]['family'],
+		"Birthdate" : self.patient_data['birthDate'],
+		"Sex" : self.patient_data['gender'].title(),
+		"Street" : self.patient_data["address"][0]["line"],
+		"City" : self.patient_data["address"][0]["city"],
+		"State" : self.patient_data["address"][0]["state"],
+		"Country" : self.patient_data["address"][0]["country"]
+		}
+
+		self.demographicsDictionary = dem_dict
+
+
+patient=Patient(id="SarahThompson")
+patient.extractPatientDemographics()
